fa22daa1ce392edae58a2a6193b97d955049dce3
BlockOG <alesiobaca@gmail.com>
2023-02-25 18:01:19 +0200

    Hmmmm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,13 +8,15 @@ module.exports = {
     ],
 
     parserOptions: {
-        // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
-        // things to lint correctly. We do not point this at "./tsconfig.json" because certain files
-        // (such at this file) should be linted but not included in the actual project output.
+        // ESLint needs to know about the project's TypeScript settings in order for
+        // TypeScript-specific things to lint correctly. We do not point this at "./tsconfig.json"
+        // because certain files (such at this file) should be linted but not included in the actual
+        // project output.
         project: "./tsconfig.eslint.json",
     },
 
     rules: {
         // Insert changed or disabled rules here, if necessary.
+        "jsdoc/check-values": "off",
     },
 };
